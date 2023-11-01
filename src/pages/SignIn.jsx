965e287c8b4fff15eb9256313d2dcaa626bd5307
Vasily Glazkov <vvg.doc@yandex.ru>
2023-11-01 15:40:37 +0200

    added firebase auth logic & users collectiion to db

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link, useNavigate} from "react-router-dom";
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -20,6 +21,24 @@ function SignIn(props) {
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const auth = getAuth();
+
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+            if (userCredential.user) {
+                navigate('/')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+
+    }
+
     return (
         <>
             <div className='pageContainer'>
@@ -27,7 +46,7 @@ function SignIn(props) {
                     <p className='pageHeader'>Welcome Back!</p>
                 </header>
                 <main>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <input
                             type="email"
                             className='emailInput'
@@ -57,7 +76,7 @@ function SignIn(props) {
                         <div className='signInBar'>
                             <p className='signInText'>Sign In</p>
                             <button className='signInButton'>
-                                <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+                                <ArrowRightIcon fill='#fff' width='34px' height='34px'/>
                             </button>
                         </div>
                     </form>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 import {Link, useNavigate} from "react-router-dom";
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
+import {setDoc, doc, serverTimestamp} from 'firebase/firestore';
+import {db} from "../firebase.config";
 import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
-function SignUp(props) {const [showPassword, setShowPassword] = useState(false);
+function SignUp(props) {
+    const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -20,6 +24,32 @@ function SignUp(props) {const [showPassword, setShowPassword] = useState(false);
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+            const user = userCredential.user;
+
+            await updateProfile(auth.currentUser, {
+                displayName: name,
+            })
+
+            const formDataCopy = {...formData};
+            delete formDataCopy.password;
+            formDataCopy.timestamp = serverTimestamp();
+
+            await setDoc(doc(db, 'users', user.uid), formDataCopy);
+
+            navigate('/');
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <>
             <div className='pageContainer'>
@@ -27,7 +57,7 @@ function SignUp(props) {const [showPassword, setShowPassword] = useState(false);
                     <p className='pageHeader'>Please, Sign Up!</p>
                 </header>
                 <main>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <input
                             type="text"
                             className='nameInput'
@@ -65,7 +95,7 @@ function SignUp(props) {const [showPassword, setShowPassword] = useState(false);
                         <div className='signUpBar'>
                             <p className='signUpText'>Sign Up</p>
                             <button className='signUpButton'>
-                                <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+                                <ArrowRightIcon fill='#fff' width='34px' height='34px'/>
                             </button>
                         </div>
                     </form>
