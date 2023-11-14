2e160fccd893249067e206e29d2adfd0e62286d0
Vasily Glazkov <vvg.doc@yandex.ru>
2023-11-14 18:20:54 +0200

    Added restore password & Google OAuth functionalities

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,8 +7,9 @@ import {setDoc, doc, serverTimestamp} from 'firebase/firestore';
 import {db} from "../firebase.config";
 import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import OAuth from "../components/OAuth";
 
-function SignUp(props) {
+function SignUp() {
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({
         name: '',
@@ -103,7 +104,7 @@ function SignUp(props) {
                         </div>
                     </form>
 
-                    {/* Google OAuth*/}
+                    <OAuth />
 
                     <Link to='/sign-in' className='registerLink'>
                         Sign In Instead
